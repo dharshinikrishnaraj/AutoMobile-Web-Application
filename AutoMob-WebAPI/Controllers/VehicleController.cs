2f23795100d565b27748b2ca0d92d8c9fca96ad2
dharshinikrishnaraj <dharshinikrishnraj@gmail.com>
2025-01-29 14:59:18 +0530

    Add project files.

---
+++ AutoMob-WebAPI/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMob_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+
+    }
+}
