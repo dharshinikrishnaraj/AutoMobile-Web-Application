5744d162ce6c2ba9d612bd659a03071b9f78efeb
dharshinikrishnaraj <dharshinikrishnraj@gmail.com>
2025-01-29 16:16:00 +0530

    Created Context class, Model, configured sql connection

---
+++ AutoMob-WebAPI/Models/VehicleDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AutoMob_WebAPI.Models
+{
+    public class VehicleDbContext : DbContext
+    {
+        public VehicleDbContext(DbContextOptions<VehicleDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<VehicleModel> Vehicles { get; set;}
+
+        //To connect to the database, configure the context class, use optionsBuilder to specify the connection string.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Employee;Integrated Security=True;Trust Server Certificate=False;");
+
+            }
+        }
+    }
+}
---
+++ AutoMob-WebAPI/Models/VehicleModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoMob_WebAPI.Models
+{
+    public class VehicleModel
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+    }
+}
